a022580394521f7dc7099326ab4ee78ff2d2aa6d
uk <ukiiing00@gmail.com>
2023-08-06 22:35:19 +0900

    CHAT WITH WEBSOCKETS

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import http from 'http';
+import { WebSocketServer } from 'ws';
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+function onSocketClose() {
+    console.log('Disconnected from the Browser ❌');
+}
+
+const sockets = [];
+
+wss.on('connection', (socket) => {
+    sockets.push(socket);
+    console.log('Connected to Browser ✅');
+    socket.on('close', onSocketClose);
+    socket.on('message', (message) => {
+        sockets.forEach((aSocket) => aSocket.send(message.toString()));
+    });
+});
+
+server.listen(3000, handleListen);
